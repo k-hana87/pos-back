310f0cf4d152b3fd3416f58df693f590c58bf527
k-hana87 <keiko.hanada.2022@gmail.com>
2025-06-06 11:49:56 +0900

    PemファイルとSSL認証の修正

--- app/database.py
+++ app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from dotenv import load_dotenv
 import os
+import urllib.parse
 
 # .envファイルから環境変数を読み込む
 load_dotenv()
@@ -13,8 +14,14 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "3306")  # デフォルト3306
 DB_NAME = os.getenv("DB_NAME")
 
+# .pemファイル名を指定
+ssl_cert = "DigiCertGlobalRootCA.crt (1).pem"
+
+# SSL接続用のパラメータをURLに追加
+query = urllib.parse.urlencode({ "ssl_ca": ssl_cert })
+
 # MySQL接続URLを構築
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?{query}"
 
 # SQLAlchemyのエンジンとセッションを作成
 engine = create_engine(DATABASE_URL, echo=True)
