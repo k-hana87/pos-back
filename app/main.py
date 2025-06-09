295d31e94e6b135b7073575dd970954a883d0796
k-hana87 <keiko.hanada.2022@gmail.com>
2025-06-10 01:07:14 +0900

    main.py　corsのorigins設定

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS設定（フロントエンドが別ドメインで動く場合）
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 本番は限定ドメインにするのが安全
+    allow_origins=["https://app-step4-36.azurewebsites.net/"],  # 本番は限定ドメインにするのが安全
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
